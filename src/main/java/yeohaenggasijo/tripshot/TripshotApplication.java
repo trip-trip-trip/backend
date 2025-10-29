1e2382c3701f4e75d782ebbf47a55185bb0c0c38
art1factia <137972596+art1factia@users.noreply.github.com>
2025-10-29 15:42:27 +0900

    init

---
+++ src/main/java/yeohaenggasijo/tripshot/TripshotApplication.java
@@ -0,0 +1,13 @@
+package yeohaenggasijo.tripshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripshotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yeohaenggasijo/tripshot/TripshotApplicationTests.java
@@ -0,0 +1,13 @@
+package yeohaenggasijo.tripshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
